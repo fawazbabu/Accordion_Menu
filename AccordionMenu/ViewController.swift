b337360af2145109b46ad7a1ac0a9f336833950a
fawaz <fawaz@lataant.com>
2015-03-05 15:12:20 +0530

    Initial Commit

---
+++ AccordionMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccordionMenu
+//
+//  Created by Lataant on 05/03/15.
+//  Copyright (c) 2015 Zaeem Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
